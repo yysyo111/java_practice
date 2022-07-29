e74ec06e7b62680332f060984234d99b2ed523f3
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-29 16:23:12 +0900

    [add]メソッド練習問題

---
+++ Java/src/method_practice01.java
@@ -0,0 +1,16 @@
+
+public class method_practice01 {
+	public static void main(String[] args) {
+		introduceOnseself();
+	}
+	public static void introduceOnseself() {
+		String name = "しょう";
+		int age = 23;
+		double height = 165;
+		char zodiac = '虎';
+		System.out.println("私の名前は" + name + "です");
+		System.out.println("歳は" + age + "です");
+		System.out.println("身長は" + height + "cmです");
+		System.out.println("十二士は" + zodiac + "です");
+	}
+}
---
+++ Java/src/method_practice03.java
@@ -0,0 +1,18 @@
+
+public class method_practice03 {
+	public static void main(String[] args) {
+		String address = "uso800@xxxx.com";
+		String text = "今度、飲みにいきませんか";
+		email(address, text);
+	}
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名： 無題");
+		System.out.println("本文：" + text);
+	}
+	public static void email(String title, String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：" + title);
+		System.out.println("本文：" + text);
+	}
+}
