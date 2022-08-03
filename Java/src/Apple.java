534d55c66f5b91bfd2c2b5d1e236c8ed4ce39f66
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-03 21:57:06 +0900

    [add]ジェネリクス

--- Java/src/Apple.java
+++ Java/src/Apple.java
@@ -1,25 +1,4 @@
 
-class Apple {
-	private String name;
+
 	
-	Apple(String name) {
-		this.name = name;
-	}
-	
-	String getName() {
-		return name;
-	}
-	
-	class Peach {
-		private String name;
-		
-		Peach(String name) {
-			this.name = name;
-		}
-		
-		String getName() {
-			return name;
-		}
-	}
-	
-} 
+
--- Java/src/Basket.java
+++ Java/src/Basket.java
@@ -1,16 +1,49 @@
 
 class Basket<E> {
-	private E elem;
-	
-	Basket(E e) {
-		elem = e;
-	}
-	
-	void replace(E e) {
-		elem = e;
-	}
-	
-	E get() {
-		return elem;
+  private E elem;
+
+  Basket(E e) {
+    elem = e;
+  }
+
+  void replace(E e) {
+    elem = e;
+  }
+
+  E get() {
+    return elem;
+  }	
+  
+  class Apple {
+		Basket<Apple> appleBasket = new Basket<>(new Apple("ふじ"));
+		
+		public void main(String[] args)  {
+			appleBasket.replace(new Apple("紅玉"));
+//			System.out.println(appleBasket.get().getName());
+			System.out.println(appleBasket.get().getName());
+		}
+		
+		private String name;
+		
+		Apple(String name) {
+			this.name = name;
+		}
+		
+		String getName() {
+			return name;
+		}
+	} 
+
+	class Peach {
+		private String name;
+		
+		Peach(String name) {
+			this.name = name;
+		}
+		
+		String getName() {
+			return name;
+		}
 	}
 }
+
