7b3de8b110be41fdf07aa2bc55b0af0bd055b2c7
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-05 15:57:36 +0900

    [add]無名クラス

--- Java/src/A.java
+++ Java/src/A.java
@@ -1,8 +1,13 @@
 
 public class A {
 	public static void main(String[] args) {
-		 A a = new A();
-	     System.out.println(a.hello());
+		 A b = new A() {
+			 @Override 
+			 public String hello() {
+				 return "Hello B!";
+			 }
+		 };
+	     System.out.println(b.hello());
 	}
 	 public String hello() {
 	     return "Hello A!";
