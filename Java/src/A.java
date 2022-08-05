cb6ddbca83d7da794ad7939602f96c3d05dad015
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-08-05 15:51:47 +0900

    [add]練習

---
+++ Java/src/A.java
@@ -0,0 +1,10 @@
+
+public class A {
+	public static void main(String[] args) {
+		 A a = new A();
+	     System.out.println(a.hello());
+	}
+	 public String hello() {
+	     return "Hello A!";
+     }
+}
